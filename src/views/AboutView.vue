ea18cff5c40b24d9beac68e3950b61d3937cfbd9
mekanio20 <mrxyok138@gmail.com>
2024-08-12 15:37:24 +0500

    about page responsive completed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -19,52 +19,52 @@
         <img src="/icons/shadow.png" class="absolute -bottom-[500px] -left-[600px] z-10">
 
         <div class="px-10 py-56 relative z-20">
-            <div class="flex flex-col items-start space-y-6 ml-10">
-                <h3 class="font-rajdhani font-medium text-2xl text-m_orange-100 uppercase">About us</h3>
-                <h2 class="text-8xl font-bold stroke-text uppercase"
+            <div class="flex flex-col items-start space-y-6 sm:ml-10">
+                <h3 class="font-rajdhani font-medium lg:text-2xl sm:text-xl text-lg text-m_orange-100 uppercase">About us</h3>
+                <h2 class="xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl text-4xl font-bold stroke-text uppercase"
                     :class="[getMode ? 'stroke-text' : 'stroke-text-black']">
                     We make your
                 </h2>
                 <div class="flex items-end">
-                    <h1 class="font-rajdhani font-bold text-8xl uppercase"
+                    <h1 class="font-rajdhani font-bold xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl text-4xl uppercase"
                         :class="[getMode ? 'text-white' : 'text-m_blue-100']">
                         Vision a reality
                     </h1>
                     <div class="rounded-full w-3 h-3 mb-4 bg-m_orange-100 animate__animated animate__bounce"></div>
                 </div>
-                <p class="font-inter font-normal text-lg text-m_gray-300 w-1/2">
+                <p class="font-inter font-normal lg:text-lg sm:text-base text-sm text-m_gray-300 md:w-1/2 w-full">
                     Parlak Nesil is a multi-disciplinary design studio. Studio creates brand identities and develops
                     strategies. We are engaged in finding the optimal solutions covering all spheres of design spectrum
                     by using a unique approach.
                 </p>
             </div>
-            <div class=" pt-20 flex flex-col items-center justify-center">
-                <img src="/icons/parlak-nesil.png" class="object-cover">
+            <div class="pt-20 flex flex-col items-center justify-center">
+                <img src="/icons/parlak-nesil.png" class="object-cover lg:w-52 sm:w-40 w-32">
             </div>
             <div class="flex flex-col items-center">
-                <h4 class="my-28 font-rajdhani font-semibold text-4xl uppercase" :class="[getMode ? 'text-white' : 'text-black']">Our team</h4>
+                <h4 class="my-28 font-rajdhani font-semibold lg:text-4xl md:text-3xl text-2xl uppercase" :class="[getMode ? 'text-white' : 'text-black']">Our team</h4>
                 <div class="flex flex-col">
                     <div class="w-full h-96 py-10 flex items-center justify-between border-m_gray-500 border-t overflow-hidden"
                         v-for="item in teams" :key="item.id" :id="item.id">
                         <div class="flex-1">
-                            <div class="font-rajdhani font-medium text-[350px]" :class="[getMode ? 'text-white' : 'text-m_blue-100']">{{ item.id }}</div>
+                            <div class="font-rajdhani font-medium lg:text-[350px] md:text-[300px] text-[200px]" :class="[getMode ? 'text-white' : 'text-m_blue-100']">{{ item.id }}</div>
                         </div>
-                        <div class="flex-1 flex flex-col">
-                            <h3 class="font-rajdhani font-semibold text-4xl uppercase mb-8" :class="[getMode ? 'text-white' : 'text-m_blue-100']">{{ item.name }}
+                        <div class="sm:flex-1 px-4 flex flex-col">
+                            <h3 class="font-rajdhani font-semibold lg:text-4xl md:text-3xl text-2xl uppercase sm:mb-8 mb-4" :class="[getMode ? 'text-white' : 'text-m_blue-100']">{{ item.name }}
                             </h3>
-                            <p class="font-inter font-normal text-lg text-m_gray-300">
+                            <p class="font-inter font-normal lg:text-lg sm:text-base text-sm text-m_gray-300">
                                 {{ item.desc }}
                             </p>
                         </div>
-                        <div class="flex-2 ml-20 mt-4 self-start">
-                            <div class="px-[22px] py-7 border-[2px] rounded-full cursor-pointer" :claas="[!getMode ? 'border-black' : 'border-white']" @click="openAccordion(item.id)">
+                        <div class="flex-2 sm:ml-20 mt-4 self-start">
+                            <div class="lg:px-[22px] lg:py-7 px-[16px] py-[22px] lg:border-[2px] border rounded-full cursor-pointer" :claas="[!getMode ? 'border-black' : 'border-white']" @click="openAccordion(item.id)">
                                 <div class="w-3 h-[1px]" :class="[getMode ? 'bg-white' : 'bg-m_blue-100']"></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="mt-20 flex items-center space-x-6">
+            <div class="mt-20 flex sm:flex-row flex-col items-center space-x-6 space-y-6">
                 <div class="flex-1">
                     <img class="object-cover" src="/imgs/team-1.png">
                 </div>
